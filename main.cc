0293ec0deaea4e9609919fd9923efe8d5867dfce
Wesley Ryder <wryder@pu1.cs.ohiou.edu>
2018-12-15 16:58:47 -0500

    added commenting

--- main.cc
+++ main.cc
@@ -2,11 +2,17 @@
 #include <algorithm>
 using namespace std;
 
-
+// Function declarations, more info can be found in the function definition
 void printQuestion();
 void printChoices();
 void getAnswer(istream& input);
 
+
+//*********************************************************************************
+// Func:        main
+// Param:       None
+// Description: run the program and call nessary functions
+//*********************************************************************************
 int main()
 {
   string answer;
@@ -17,6 +23,12 @@ int main()
   return(0);
 }
 
+
+//*********************************************************************************
+// Func:        printQuestion
+// Param:       None
+// Description: Prints the question
+//*********************************************************************************
 void printQuestion()
 {
   cout << "\nOne question Quiz ;) -- " << "(Enter ABCD key to answer)" << endl;
@@ -24,12 +36,24 @@ void printQuestion()
   cout << "program\n"<< endl;
 }
 
+//*********************************************************************************
+// Func:        printChoices
+// Param:       None
+// Description: Prints the avaliable options to choose from in this case A, B, C
+//              or D.
+//*********************************************************************************
 void printChoices()
 {
   cout << "A) A time machine program \nB) Hello World program";
   cout << "\nC) Calculator program \nD) None of the above" << endl;
 }
 
+//*********************************************************************************
+// Func:        getAnswer
+// Param:       input -> istream
+// Description: Take inputed choice from user, based on that choice tell the user
+//              if they are correct
+//*********************************************************************************
 void getAnswer(istream& input)
 {
 
@@ -39,8 +63,8 @@ void getAnswer(istream& input)
 
   // Using the transform func to uppercase string (from algorithms lib)
   transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
-  cout << answer << endl;
-
+  
+  // Depending on input do the following
   if(answer == "D")
   {
     cout << "Correct! get it? because it's Hello World not Hello Word ;)" << endl;
