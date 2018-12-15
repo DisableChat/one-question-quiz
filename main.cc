e79e29a909c4b278936b0f941240e8885beb4b71
Wesley Ryder <wryder@pu1.cs.ohiou.edu>
2018-12-15 16:47:46 -0500

    removed printResult and just made everything function in getAnswer(). Full functionality now.

--- main.cc
+++ main.cc
@@ -1,27 +1,25 @@
 #include <iostream>
-#include <cctype>
+#include <algorithm>
 using namespace std;
 
 
 void printQuestion();
 void printChoices();
-char getAnswer(istream& input);
-void printResult(char answer);
+void getAnswer(istream& input);
 
 int main()
 {
-  char answer;
+  string answer;
   printQuestion();
   printChoices();
-  answer = getAnswer(cin);
-  printResult(answer);
+  getAnswer(cin);
 
   return(0);
 }
 
 void printQuestion()
 {
-  cout << "One question Quiz ;) -- " << "(Enter ABCD key to answer)" << endl;
+  cout << "\nOne question Quiz ;) -- " << "(Enter ABCD key to answer)" << endl;
   cout << "A program that outputs 'Hello Word' is usually called a _______ ";
   cout << "program\n"<< endl;
 }
@@ -32,15 +30,25 @@ void printChoices()
   cout << "\nC) Calculator program \nD) None of the above" << endl;
 }
 
-char getAnswer(istream& input)
+void getAnswer(istream& input)
 {
-  char answer;
+
+  string answer;
   cout << "Choice: ";
   input >> answer;
-  answer = toupper(answer);
-  if(answer == 'A' || answer == 'B' || answer == 'C' || answer == 'D')
+
+  // Using the transform func to uppercase string (from algorithms lib)
+  transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
+  cout << answer << endl;
+
+  if(answer == "D")
   {
-    return answer;
+    cout << "Correct! get it? because it's Hello World not Hello Word ;)" << endl;
+  }
+  else if(answer == "A" || answer == "B" || answer == "C")
+  {
+    cout << "Incorrect try again!" << endl;
+    getAnswer(cin);
   }
   else
   {
@@ -49,16 +57,3 @@ char getAnswer(istream& input)
   }
 }
 
-void printResult(char answer)
-{
-  if(answer == 'D')
-  {
-    cout << "Correct! get it? because it's Hello World not Hello Word ;)" << endl;
-  }
-  else
-  {
-    cout << "Incorrect try again!" << endl;
-    answer = getAnswer(cin);
-    printResult(answer);
-  }
-}
