6fa6d0dcf77e1d8b57e1b2c8a24999fd06e4e0c8
MagikarpUsedFly <wr140815@ohio.edu>
2018-12-18 20:04:18 -0500

    included handling for piping an input when running the program (previously causing a segfualt)

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ using namespace std;
 // Function declarations, more info can be found in the function definition
 void printQuestion();
 void printChoices();
-void getAnswer(istream& input);
+void getAnswer(istream& input, int inputCounter);
 
 
 //*********************************************************************************
@@ -15,10 +15,11 @@ void getAnswer(istream& input);
 //*********************************************************************************
 int main()
 {
+  int inputCounter = 0;
   string answer;
   printQuestion();
   printChoices();
-  getAnswer(cin);
+  getAnswer(cin, inputCounter);
 
   return(0);
 }
@@ -50,34 +51,40 @@ void printChoices()
 
 //*********************************************************************************
 // Func:        getAnswer
-// Param:       input -> istream
+// Param:       input, from user
+//              inputCounter, makes sure that user doesn't keep entering too many
+//              incorrect answers due to recursion and the stack
 // Description: Take inputed choice from user, based on that choice tell the user
 //              if they are correct
 //*********************************************************************************
-void getAnswer(istream& input)
+void getAnswer(istream& input, int inputCounter)
 {
-
   string answer;
   cout << "Choice: ";
   input >> answer;
 
   // Using the transform func to uppercase string (from algorithms lib)
   transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
-  
+
   // Depending on input do the following
   if(answer == "D")
   {
     cout << "Correct! get it? because it's Hello World not Hello Word ;)" << endl;
   }
+  else if(inputCounter >= 10)
+  {
+    cout << "\nInvalid/incorrect choice 10 times now, we are done here. Big Yikes!" << endl;
+  }
   else if(answer == "A" || answer == "B" || answer == "C")
   {
     cout << "Incorrect try again!" << endl;
-    getAnswer(cin);
+    inputCounter++;
+    getAnswer(cin, inputCounter);
   }
   else
   {
     cout << "Invalid choice!" << endl;
-    getAnswer(cin);
+    inputCounter++;
+    getAnswer(cin, inputCounter);
   }
 }
-
